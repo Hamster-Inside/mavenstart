672d80be450940a96e3c5a35515867c61a6f347d
Arek <abudkowski@interia.pl>
2020-03-07 13:11:16 +0100

    kolejne zmiany

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -1,10 +1,24 @@
 package io.github.mat3e;
 
 
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.Server;
+   Server server = new Server(8080);
+           server.setHandler(new HelloWorld());
+
+           server.start();
+           server.join();
+
 public class App {
 
 
     public static void main(String[] args) {
+        var webapp = new WebAppContext();
+      //  webapp.addServlet(HelloServlet.class, pathSpec: "/api/*");
+        var server = new Server(8080);
+        server.setHandler(webapp);
 
+        server.start();
+        server.join();
     }
 }
--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -3,11 +3,14 @@ package io.github.mat3e;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+
+@WebServlet(name = "Hello", urlPatterns = {"/api/*"})
 
 public class HelloServlet extends HttpServlet {
 
