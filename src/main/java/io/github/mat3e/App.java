cedd4a00084021d1a3298ad83e45e1b03d2f895d
Arek <abudkowski@interia.pl>
2020-03-07 14:34:38 +0100

    ehhh problemy

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -1,20 +1,32 @@
 package io.github.mat3e;
 
 
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.webapp.*;
 import org.eclipse.jetty.server.Server;
-   Server server = new Server(8080);
-           server.setHandler(new HelloWorld());
-
-           server.start();
-           server.join();
 
 public class App {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         var webapp = new WebAppContext();
       //  webapp.addServlet(HelloServlet.class, pathSpec: "/api/*");
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]
+                {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration()
+                });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
         var server = new Server(8080);
         server.setHandler(webapp);
 
