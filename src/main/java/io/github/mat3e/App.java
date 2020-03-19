5a0f1463b96ef2575a3199725bbe6a941d2561fb
Arek <abudkowski@interia.pl>
2020-03-19 18:08:24 +0100

    dodane hibernate

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -4,15 +4,19 @@ package io.github.mat3e;
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.webapp.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.hibernate.SessionFactory;
+
 public class App {
 
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         var webapp = new WebAppContext();
-      //  webapp.addServlet(HelloServlet.class, pathSpec: "/api/*");
+        //  webapp.addServlet(HelloServlet.class, pathSpec: "/api/*");
         webapp.setResourceBase("src/main/webapp");
         webapp.setContextPath("/");
         webapp.setConfigurations(new Configuration[]
@@ -29,7 +33,12 @@ public class App {
         webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
         var server = new Server(8080);
         server.setHandler(webapp);
-
+        server.addLifeCycleListener(new AbstractLifeCycle.AbstractLifeCycleListener() {
+            @Override
+            public void lifeCycleStopped(LifeCycle event) {
+                HibernateUtil.close();
+            }
+        });
         server.start();
         server.join();
     }
