0e3b43cbc6dffbd798b44cb50602d9c0f17fb0c9
Arek <abudkowski@interia.pl>
2020-05-18 20:33:29 +0200

    wykonane zadanie 73.7

--- src/main/java/io/github/mat3e/todo/TodoRepository.java
+++ src/main/java/io/github/mat3e/todo/TodoRepository.java
@@ -1,6 +1,7 @@
 package io.github.mat3e.todo;
 
 import io.github.mat3e.HibernateUtil;
+import io.github.mat3e.lang.Lang;
 import io.github.mat3e.todo.Todo;
 
 import java.util.List;
@@ -12,6 +13,15 @@ public class TodoRepository {
         var session = HibernateUtil.getSessionFactory().openSession();
         var transaction = session.beginTransaction();
         var result = session.createQuery("from Todo", Todo.class).list();
+        transaction.commit();
+        session.close();
+        return result;
+    }
+    Todo toggleTodo (Integer id){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+       var result = session.get(Todo.class, id);
+       result.setDone(!result.isDone());
         transaction.commit();
         session.close();
         return result;
--- src/main/java/io/github/mat3e/todo/TodoServlet.java
+++ src/main/java/io/github/mat3e/todo/TodoServlet.java
@@ -46,4 +46,18 @@ public class TodoServlet extends HttpServlet {
 
 
     }
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+       var pathInfo = req.getPathInfo();
+        try {
+            var todoId = Integer.valueOf(pathInfo.substring(1));
+            var todo = repository.toggleTodo(todoId);
+            resp.setContentType("application/json;charset=UTF-8");
+            mapper.writeValue(resp.getOutputStream(), todo);
+        } catch (NumberFormatException e){
+            logger.warn("Wrong path used: " + pathInfo);
+
+        }
+
+    }
 }
